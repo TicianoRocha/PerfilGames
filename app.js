8d2e45e0860bae39d84e6c7e3d3cc4601316db80
TicianoRocha <ticirocha@proton.me>
2022-09-14 18:46:44 -0300

    g

---
+++ app.js
@@ -0,0 +1,6 @@
+function Enviar() {
+    var email = document.getElementById("emailid");
+    if (email.value != "") {
+        alert('Obrigado sr(a) ' + email.value + ' os seus dados foram encaminhados com sucesso');
+    }
+} 
